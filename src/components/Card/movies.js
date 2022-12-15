afe22f24236a328b11deddc4d1de863052ebd185
Theneau Maxime <theneau.maxime@gmail.com>
2022-12-15 16:01:30 +0100

    feat(Components): Created toogle dislike

--- src/components/Card/movies.js
+++ src/components/Card/movies.js
@@ -14,12 +14,25 @@ function Movies({
   const categoryData = ["Animation", "Comedy", "Drame", "Thriller"];
   const handleClickLike = (likesEvent, idEvent) => {
     setNewLikes(!newLikes);
-    console.log(likesEvent, idEvent);
     const newMovies = moviesState.map((movie) => {
       if (movie.id === idEvent) {
         return {
           ...movie,
           likes: newLikes ? likesEvent + 1 : likesEvent - 1,
+        };
+      }
+      return movie;
+    });
+    dispatch(newMoviesActionAction(newMovies));
+  };
+
+  const handleClickDislike = (dislikesEvent, idEvent) => {
+    setNewLikes(!newLikes);
+    const newMovies = moviesState.map((movie) => {
+      if (movie.id === idEvent) {
+        return {
+          ...movie,
+          dislikes: newLikes ? dislikesEvent + 1 : dislikesEvent - 1,
         };
       }
       return movie;
@@ -39,10 +52,14 @@ function Movies({
           <BiLike />
           <span>{likes}</span>
         </button>
-        <div className="cards__movies__likes__item">
+        <button
+          className="cards__movies__likes__item"
+          type="button"
+          onClick={() => handleClickDislike(dislikes, id)}
+        >
           <BiDislike />
           <span>{dislikes}</span>
-        </div>
+        </button>
       </div>
       <select className="cards__movies__select" multiple>
         {categoryData.map((categoryItem) => (
